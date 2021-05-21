8fd91b7ced8a2ad29dd886d4411aa7b6d5ff1326
Taylor Smith <tbsmith0205@gmail.com>
2021-05-21 09:19:17 -0600

    set up dependencies, server listener, and basic route paths in server.js

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
+// set up express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/assets/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/assets/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const db = JSON.parse(data);
+    // empty array for new data that has been parsed.
+    const newDB = [];
+  });
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
